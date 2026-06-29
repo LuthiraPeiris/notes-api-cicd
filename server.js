31c227c67c909c04e2af9f2980cdc35ac840247d
Luthira Peiris <22cse0369@ms.sab.ac.lk>
2026-06-29 07:56:54 +0530

    updated the code

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/health", (req, res) => {
-  res.json({ status: "ok", message: "Server is healthy" });
+  res.json({ status: "ok", message: "Server is healthy from CI/CD pipeline" });
 });
 
 app.get("/api/notes", (req, res) => {
